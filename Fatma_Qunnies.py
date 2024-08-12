ac48c7a58cc90438ba0637394c73a705ec5bd032
fatmaQunnies <fatima.n.qunnies@gmail.com>
2024-08-12 13:27:29 +0300

    Filter Books by Author

--- Fatma_Qunnies.py
+++ Fatma_Qunnies.py
@@ -15,3 +15,13 @@ books = [
         "author":"Yousef Amr",
         "availability": True,
         "tags": ["Data","Python","Intermediate"]}]
+# Step 2: Filter Books by Author
+available_books_by_mohammed = []
+for book in books:
+    if "Mohammed" in book["author"] :
+        book["availability"] = True
+        available_books_by_mohammed.append(book["title"])
+        print(book["availability"])
+
+# print 
+print(available_books_by_mohammed)
