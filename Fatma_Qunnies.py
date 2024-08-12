7b042c86746cea82177ffa4068f1add17c090147
fatmaQunnies <fatima.n.qunnies@gmail.com>
2024-08-12 13:08:08 +0300

    add data to books list

---
+++ Fatma_Qunnies.py
@@ -0,0 +1,17 @@
+#list ,Create a list consisting of dictionary 
+books = [
+    {"title": "Python Fundamentals",#dict have key and value
+        "author": "Ahmed Zayed",
+        "availability": True,
+        "tags":["Programming", "Python", "Basics"]#dictionary have a list of tags
+        },
+    {
+        "title":"The Art of Coding",
+        "author":"Mohammed Ali",
+        "availability": False,
+        "tags": ["Coding", "Art","Creativity"]
+    },{
+        "title":"Data Structures in Python",
+        "author":"Yousef Amr",
+        "availability": True,
+        "tags": ["Data","Python","Intermediate"]}]
