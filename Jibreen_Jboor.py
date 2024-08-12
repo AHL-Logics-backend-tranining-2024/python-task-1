9ee895bb047671188ec9b6c28d1d7065c6017477
Jibreen <jibreenjboor@gmail.com>
2024-08-12 19:46:50 +0300

    I added comments to clarify the actions taken

--- Jibreen_Jboor.py
+++ Jibreen_Jboor.py
@@ -1,4 +1,7 @@
+
+# Step 1: Initialize a list of books, each represented as a dictionary with title, author, availability, and tags.
 books = [
+    # Dictionary with Multiple Properties
     {
         "title": "Python Fundamentals",
         "author": "Ahmed Zayed",
@@ -19,11 +22,15 @@ books = [
     },
 ]
 
+#############################################################
 
+# Step 2: Filter Books by Author and Availability
 available_books_by_mohammed = [book["title"] for book in books if "Mohammed" in book["author"] and book["availability"]];
 
+# Print
 print(F"available books by mohammed is : {available_books_by_mohammed}");
-#Output :[available books by mohammed is : []]
+# Output :[available books by mohammed is : []]
+
 
 # Check if the list is empty
 if not available_books_by_mohammed :
@@ -32,24 +39,32 @@ else:
     for ob in available_books_by_mohammed:
         print(ob);
 
-#Output :[The list is empty, nothing to analyze.]
+# Output :[The list is empty, nothing to analyze.]
 
-###############################
 
+
+#############################################################
+
+# Step 3: Analyze Tags with Match Statement
 for book in books:
+   # Create a Variable to Store Data from a Book Object
    first_tag = book["tags"][0];
 
    match first_tag:
+       # Check if the first tag is "Programming" and print a message.
        case "Programming":
            print(f"The book {book['title']} is related to {first_tag}.")
+       # Check if the first tag is "Coding" and print a message.
        case "Coding":
            print(f"The book {book['title']} is related to {first_tag}.")
+       # Check if the first tag is "Data" and print a message.
        case "Data":
            print(f"The book {book['title']} is related to {first_tag}.")
+       # Handle any unrecognized tags and print the message.
        case _:
            print(f"'{book['title']}' has an unrecognized tag: {first_tag}.")
 
-
+# Output when all tags is defined
 """ 
 The book Python Fundamentals is related to Programming.
 The book The Art Of Coding is related to Coding.
